10865e889e49654738bd69098de2a0a7a713dcf4
aughade <akashughade77@gmail.com>
2024-07-11 23:41:13 +0530

    feature updated update cli through version

--- Python-Script/main.py
+++ Python-Script/main.py
@@ -1,32 +1,49 @@
 import yaml
+import json
+import subprocess
+import time
 
-# Define the data for the YAML file
-data = {
-  "sources": {
-    "cluster_autoscaler_source": {
-      "kind": "helmchart",
-      "pattern": "*",
-      "spec": {
-        "name": "ingress-nginx",
-        "url": "https://kubernetes.github.io/ingress-nginx",
-        "version": "latest"
-      }
-    }
-  },
-  "targets": {
-    "update_cluster_autoscaler_version": {
-      "name": "Update cluster-autoscaler version in input.tf",
-      "kind": "terraform/file",
-      "spec": {
-        "file": "Template/EKS/input.tf",
-        "path": "variable.NGINX_CHART_VERSION.default"
-      }
-    }
-  }
-}
 
-# Open the file for writing in YAML format
-with open(".github/updatecli.yaml", "w") as f:
-  yaml.dump(data, f, default_flow_style=False)
 
-print("YAML file generated: .github/updatecli.yaml")
+YAML_FILE = '.github/config/chart.yaml'
+JSON_FILE = '.github/chart.json'
+
+TEMPLATEDATA = { "sources": { "helm": { "kind": "helmchart", "pattern": "*", "spec": { "name": "cluster-autoscaler", "url": "https://kubernetes.github.io/autoscaler", "version": "latest" } } }, "targets": { "update_version": { "name": "Update version in input.tf", "kind": "terraform/file", "spec": { "file": "Template/EKS/input.tf", "path": "variable.cluster-autoscaler.default" } } } }
+
+
+def load_json(file):
+    with open(file, "r") as f:
+        data = json.load(f)    
+    return data
+
+
+def load_yml(file):
+    with open(file, "r") as f:
+        data = yaml.safe_load(f)
+    return data
+
+def write_yml(file, data):
+    with open(file, 'w+') as ff:
+        yaml.dump_all([data], ff)
+
+
+yml_data = load_yml(YAML_FILE)
+
+
+json_data = load_json(JSON_FILE)
+loaded_data = {}
+for json_chart in json_data['charts']:
+    for yaml_chart in yml_data['charts']:
+        if yaml_chart['chart'] == json_chart['chart']:
+            loaded_data[json_chart['chart']] = {'repo': yaml_chart['repository'], 'tf_var_name': json_chart['tf_version_var_name']}
+
+for chart_name, item in loaded_data.items():
+    TEMPLATEDATA['sources']['helm']['spec']['name'] = chart_name
+    TEMPLATEDATA['sources']['helm']['spec']['url'] = item['repo']
+    TEMPLATEDATA['targets']['update_version']['spec']['path'] = f"variable.{item['tf_var_name']}.default"
+    filename = "updatecli-generated.yml"
+    write_yml(filename, TEMPLATEDATA)
+    command = ["updatecli", "apply", "--config", f"{filename}"]
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+    print(proc)
+    time.sleep(3)
